3a3bace8f0519fa15345ae0fc557fbe443c0d185
andrezlozano <andreslozanohu@gmail.com>
2022-09-28 00:02:50 -0500

    Se agrega el mensaje en el Main

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+
+    //define("BASE_URL", "http://localhost/proverMotos/");
+    const BASE_URL = "http://localhost/proverMotos/";
+    const LIBS = "Libraries/";
+    const VIEWS = "Views/";
+
+?>
