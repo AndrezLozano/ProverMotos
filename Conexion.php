46951f57bd0f2febfbb467fb34e3b2aed7550bd0
andrezlozano <andreslozanohu@gmail.com>
2022-09-27 19:28:42 -0500

    Se agrega el Proyecto

---
+++ Conexion.php
@@ -0,0 +1,24 @@
+<?php
+
+class Conexion {
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "db_provermotos";
+    private $conect;
+
+    public function __construct(){
+        $connectionString = "mysql:hos=".$this->host.";dbname=".$this->db.";charset=utf8";
+        try {
+            $this->conect = new PDO($connectionString,$this->user,$this->password);
+            $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión Exitosa";
+        } catch (Exception $e) {
+            $this->conect = 'Error de conexión';
+            echo "ERROR: ". $e->getMessage();
+        }
+    }
+}
+
+$conect = new Conexion();
+?>
